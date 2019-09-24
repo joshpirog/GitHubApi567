0f7f00e02c5ad90cdfafed3bb501f87de84b6983
joshpirog <52870102+joshpirog@users.noreply.github.com>
2019-09-24 17:42:31 -0400

    Add files via upload

---
+++ GitHubApi.py
@@ -0,0 +1,64 @@
+'''
+Created on Sep 21, 2019
+
+@author: Josh
+'''
+
+
+import requests
+import json
+
+userId = input("Enter GitHub user ID: ")
+
+
+
+def githubInfo(userId):
+    repo = requests.get('https://api.github.com/users/' + userId + '/repos')
+    repoJson = repo.json() 
+    listOfRepos = []
+    toString = ''
+    
+    for repo in repoJson:
+        
+        commits = requests.get('https://api.github.com/repos/' + userId + '/' + repo["name"] + '/commits')
+        commitsJson = commits.json()
+        commitsLen = len(commitsJson)
+        
+        #print("Repo " + repo["name"] + " has " + str(commitsLen) + " commits.")
+        #listOfRepos.append("Repo " + repo["name"] + " has " + str(commitsLen) + " commits.")
+        toString += "Repo " + repo["name"] + " has " + str(commitsLen) + " commits." + '\n'
+
+    #print(toString)
+    return toString
+
+
+
+print(githubInfo(userId))
+# listOfRepos = githubInfo(userId)
+# 
+# for repo in listOfRepos:
+#     print(repo)
+
+
+
+
+
+
+
+
+# HEADERS = {
+#     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
+# }
+# url = 'http://stats.nba.com/stats/playbyplayv2?EndPeriod=10&EndRange=55800&GameID=0021500281&RangeType=2&Season=2016-17&SeasonType=Regular+Season&StartPeriod=1&StartRange=0'
+# response = requests.get(url, timeout=5, headers=HEADERS)
+# print(response.text)
+
+
+# #https://api.github.com/users/richkempinski/repos
+# 
+# #https://api.github.com/repos/richkempinski/hellogitworld/commits
+# 
+# #https://api.github.com/repos/richkempinski/hellogitworld/commits
+# r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+# 
+# # print(r.text)
---
+++ GitHubApi_Test.py
@@ -0,0 +1,25 @@
+import unittest
+
+from GitHubApi import githubInfo
+
+# This code implements the unit test functionality
+# https://docs.python.org/3/library/unittest.html has a nice description of the framework
+
+class TestgithubInfo(unittest.TestCase):
+    # define multiple sets of tests as functions with names that begin
+    
+    def testgithubInfo(self):
+        self.assertEqual(githubInfo("joshpirog"),'Repo ClassifyTriangle has 2 commits.\nRepo CS-385-Algorithms has 2 commits.\nRepo dribbble2react has 30 commits.\nRepo GitHubApi567 has 2 commits.\nRepo helloworld has 2 commits.\nRepo TriangleTest has 9 commits.\n')
+    
+    def testValidInput(self):
+        self.assertEqual(githubInfo(201, 500, -5), 'InvalidInput')
+        
+    def testIsIntegers(self):
+        self.assertEqual(githubInfo("apple", "pear", "fig"), 'InvalidInput')
+    
+    
+        
+    
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main(exit=False, verbosity=2)
