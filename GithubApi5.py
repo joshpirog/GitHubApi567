4d3b8223ab3997d717fe6619a23bb8771e54950c
joshpirog <52870102+joshpirog@users.noreply.github.com>
2019-10-13 20:26:10 -0400

    Add files via upload

---
+++ GithubApi5.py
@@ -0,0 +1,16 @@
+"@author: Joshua Pirog"
+
+import requests
+import json
+
+ 
+def getRepos(userId):
+    repos = requests.get("https://api.github.com/users/" + userId + "/repos")
+    repos = repos.json()
+    return repos
+
+def getCommits(userId, repoName):
+    commitUrl = requests.get("https://api.github.com/repos/" + userId + "/" + repoName+ "/commits")
+    commitUrl = commitUrl.json()
+    return len(commitUrl)
+
